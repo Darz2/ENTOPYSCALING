fc36c9b5c2b0bd09aefa32fb8185c27c16eb3b72
D.Raju <d.raju@tudelft.nl>
2024-10-26 11:41:40 +0200

    Increased the Maxpart to 1000

--- GIT.sh
+++ GIT.sh
@@ -3,7 +3,7 @@
 # echo "# ENTOPYSCALING" >> README.md
 # git init
 git add *
-git commit -m "Changes Ammended after checking the old and new executables"
+git commit -m "Increased the Maxpart to 1000"
 git branch -M main
 git remote add origin https://github.com/Darz2/ENTOPYSCALING.git
 git push -u origin main
