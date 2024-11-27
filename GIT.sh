dbe3a45d9172bc684191de172fb869f221af9c5d
D.Raju <d.raju@tudelft.nl>
2024-11-27 12:08:09 +0100

    Gitlab_TU delft

--- GIT.sh
+++ GIT.sh
@@ -6,4 +6,5 @@ git add *
 git commit -m "Gitlab_TU delft"
 # git branch -M main
 # git remote add origin https://github.com/Darz2/ENTOPYSCALING.git
+git remote add origin https://gitlab.tudelft.nl/darshanraju/ENTOPYSCALING.git
 git push -u origin main
